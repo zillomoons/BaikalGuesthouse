d621e3218b5f959332c4b995abf396a2fa80e629
zillomoons <dlzht73@ya.ru>
2021-01-23 12:03:59 +0300

    added styles to hero section

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const menuBtn = document.getElementById('toggleBtn');
 const menuOverlay = document.querySelector('.menu_container');
 
-
+// Open mobile menu
 menuBtn.addEventListener('click', function () {
     menuOverlay.classList.toggle('open');
     menuBtn.classList.toggle('active');
 
 })
+// Checks whether to collapse open mobile menu
 function checkMenu() {
     if (visualViewport.width > 640 && menuOverlay.classList.contains('open')) {
         menuOverlay.classList.remove('open');
         menuBtn.classList.remove('active');
     }
 }
+// If viewport expanding collapse open mobile menu
 window.addEventListener('resize', checkMenu);
+
+// If scroll -- sticky header
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
 
